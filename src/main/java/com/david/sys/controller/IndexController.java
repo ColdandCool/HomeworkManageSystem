cffadae6c919368ee7184447d66f6f31ea4c61f7
ColdandCool <a1600783592@gmail.com>
2017-10-16 08:48:48 +0800

    weather API just like http://localhost:8080/homework/weather/2147714

--- src/main/java/com/david/sys/controller/IndexController.java
+++ src/main/java/com/david/sys/controller/IndexController.java
@@ -1,14 +1,17 @@
 package com.david.sys.controller;
 
 import com.david.common.BaseController;
+import com.david.common.utils.HttpClientUtil;
 import com.david.sys.entity.Resource;
 import com.david.sys.service.ResourceService;
 import com.david.sys.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Set;
@@ -27,6 +30,23 @@ public class IndexController extends BaseController {
     @Autowired
     private UserService userService;
 
+    /**
+     * according city ID to show Weather API
+     *
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/weather/{id}", method = RequestMethod.GET)
+    public String getWeather(@PathVariable String id) {
+        try {
+            Integer.parseInt(id);
+        } catch (Exception e) {
+            id = "2147714";
+        }
+        String url = "http://api.openweathermap.org/data/2.5/weather?id=" + id + "&appid=43aacfb6fa831b24200a39d2d4351449";
+        return HttpClientUtil.doGet(url);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
         Set<String> permissions = userService.findPermissions();
@@ -39,5 +59,4 @@ public class IndexController extends BaseController {
     public String welcome(Model model) {
         return "welcome";
     }
-
 }
